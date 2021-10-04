bf844e7555594a71110fac76c7e8c72b2883a1cc
PearlKang <rksk1588@gmail.com>
2021-10-04 22:06:27 +0900

    2021.10.04
    2609 - GCD (Greatest Common Divisor)
    최대공약수

--- Practice/BaekJoon/2609/Main.java
+++ Practice/BaekJoon/2609/Main.java
@@ -7,24 +7,23 @@ class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		StringTokenizer st = null;
 		st = new StringTokenizer(br.readLine());
-		
+
 		int A = Integer.parseInt(st.nextToken());
 		int B = Integer.parseInt(st.nextToken());
-		
+
 		if((A > 0 && A <= 10000) && (B > 0 && B <= 10000)) {
 			int GCD = gcd(A, B);
 			bw.write(GCD + "\n");
 			bw.write(A * B / GCD + "\n");
 		}
-		
 		br.close();
 		bw.flush();
 		bw.close();
 	}
-	
+
 	public static int gcd(int a, int b) {
 		if(b == 0)
 			return a;
