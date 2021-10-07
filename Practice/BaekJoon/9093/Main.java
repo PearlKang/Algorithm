84eaa365377cc44ebc27286b430c4d27e53caca0
PearlKang <rksk1588@gmail.com>
2021-10-08 00:58:28 +0900

    2021.10.08
    9093 - reverse

---
+++ Practice/BaekJoon/9093/Main.java
@@ -0,0 +1,45 @@
+package N9093;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = null;
+		st = new StringTokenizer(br.readLine());
+
+		int T = Integer.parseInt(st.nextToken());
+		
+		for(int t = 0; t < T; t++) {
+			String sentense = br.readLine();
+			
+			if(sentense.length() <= 1000) {
+				st = new StringTokenizer(sentense);
+				
+				while(st.hasMoreTokens()) {
+					String tmp = st.nextToken();
+					if(tmp.length() <= 20)
+						bw.write(String.valueOf(reverse(tmp)) + " ");
+				}
+				bw.write("\n");
+			}
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+	
+	public static char[] reverse(String input) {
+		char[] output = new char[input.length()];
+		
+		for(int i = 0; i < input.length(); i++) {
+			output[i] = input.charAt(input.length() - i - 1);
+		}
+		
+		return output;
+	}
+}
