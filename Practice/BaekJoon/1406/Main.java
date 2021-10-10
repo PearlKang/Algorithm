cbec76ed42fda90e332aeeec1304ad9d77c316e4
PearlKang <rksk1588@gmail.com>
2021-10-10 16:34:07 +0900

    2021.10.10
    1406 - 2

--- Practice/BaekJoon/1406/Main.java
+++ Practice/BaekJoon/1406/Main.java
@@ -7,13 +7,12 @@ class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		StringTokenizer st = null;
 		st = new StringTokenizer(br.readLine());
-		
+
 		String input = st.nextToken();
 		int N = input.length();
-
 
 		if(N <= 100000) {
 			st = new StringTokenizer(br.readLine());
@@ -30,11 +29,11 @@ class Main {
 					switch(st.nextToken()) {
 					case "L":
 						if(cursor != 0)
-							tmp = input.substring(0, cursor-1) + "*" + input.substring(cursor-1); 
+							tmp = input.substring(0, cursor-1) + "*" + input.substring(cursor-1);
 						break;
 					case "D":
 						if(cursor != input.length())
-							tmp = input.substring(0, cursor+1) + "*" + input.substring(cursor+1); 
+							tmp = input.substring(0, cursor+1) + "*" + input.substring(cursor+1);
 						break;
 					case "B":
 						if(cursor != 0) {
@@ -56,44 +55,3 @@ class Main {
 		bw.close();
 	}
 }
-
-/*
-input : abcdefghi
-tmp   : abcdefghi*
-0 1 2 3 4 5 6 7 8 9
-a b c d e f g h i *
-
-//int cursor = tmp.indexOf("*");
-int cursor = 0;
-
-String tmp2 = input.substring(0,0) + "*" + input.substring(0); 
-
-String firstWord = st.nextToken();
-int N = firstWord.length();
-
-LinkedList<Character> input = new LinkedList<>();
-for(int i = 0; i < N; i++)
-	input.add(firstWord.charAt(i));
-
-if(N <= 100000) {
-	st = new StringTokenizer(br.readLine());
-	int M = Integer.parseInt(st.nextToken());
-	
-	if(M >= 1 && M <= 500000) {
-		st = new StringTokenizer(br.readLine());
-		
-		for(int m = 0; m < M; m++) {
-			switch(st.nextToken()) {
-			case "L":
-				break;
-			case "D":
-				break;
-			case "B":
-				break;
-			case "P":
-				break;
-			}
-		}
-	}
-}
-*/
