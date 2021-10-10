9483a8f6e8db87e36c1bf4217f84f5f995871088
PearlKang <rksk1588@gmail.com>
2021-10-10 17:48:47 +0900

    2021.10.10
    1406 - change substring > stack

--- Practice/BaekJoon/1406/Main.java
+++ Practice/BaekJoon/1406/Main.java
@@ -7,47 +7,51 @@ class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		
 		StringTokenizer st = null;
 		st = new StringTokenizer(br.readLine());
-
+		
 		String input = st.nextToken();
 		int N = input.length();
+		
+		Stack<Character> lStack = new Stack<Character>();
+		Stack<Character> rStack = new Stack<Character>();
+		
+		for(int i = 0; i < N; i++)
+			lStack.push(input.charAt(i));
 
 		if(N <= 100000) {
 			st = new StringTokenizer(br.readLine());
 			int M = Integer.parseInt(st.nextToken());
 
 			if(M >= 1 && M <= 500000) {
-				String tmp = input + "*";
-
 				for(int m = 0; m < M; m++) {
-					int cursor = tmp.indexOf("*");
-
 					st = new StringTokenizer(br.readLine());
-
+					
 					switch(st.nextToken()) {
 					case "L":
-						if(cursor != 0)
-							tmp = input.substring(0, cursor-1) + "*" + input.substring(cursor-1);
+						if(lStack.size() != 0)
+							rStack.push(lStack.pop());
 						break;
 					case "D":
-						if(cursor != input.length())
-							tmp = input.substring(0, cursor+1) + "*" + input.substring(cursor+1);
+						if(rStack.size() != 0)
+							lStack.push(rStack.pop());
 						break;
 					case "B":
-						if(cursor != 0) {
-							tmp = input.substring(0, cursor-1) + "*" + input.substring(cursor);
-							input = tmp.replace("*", "");
-						}
+						if(lStack.size() != 0)
+							lStack.pop();
 						break;
 					case "P":
-						tmp = input.substring(0, cursor) + st.nextToken() + "*" + input.substring(cursor);
-						input = tmp.replace("*", "");
+						lStack.push(st.nextToken().charAt(0));
 						break;
 					}
 				}
-				bw.write(input + "\n");
+				while(!lStack.isEmpty()) {
+					rStack.push(lStack.pop());
+				}
+				while(!rStack.isEmpty()) {
+					bw.write(rStack.pop());
+				}
 			}
 		}
 		br.close();
