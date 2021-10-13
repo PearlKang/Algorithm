accbfdc1ee994572fa0569f292f6bb4490fb31a4
PearlKang <rksk1588@gmail.com>
2021-10-14 01:03:06 +0900

    2021.10.14
    1550 - 16 > 10

--- Practice/BaekJoon/1550/Main.java
+++ Practice/BaekJoon/1550/Main.java
@@ -8,17 +8,12 @@ class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		//input : 16, hexa
 		StringTokenizer st = null;
 		st = new StringTokenizer(br.readLine());
 		
-		//output1 : 10, decimal
-		int tmp10 = Integer.parseInt(st.nextToken(),16);
-		//output2 : 16, hex
-		String tmp16 = Integer.toHexString(tmp10);
+		int input = Integer.parseInt(st.nextToken(),16);
 		
-		bw.write(tmp10 + "\n");
-		bw.write(tmp16 + "\n");
+		bw.write(input + "\n");
 		
 		br.close();
 		bw.flush();
