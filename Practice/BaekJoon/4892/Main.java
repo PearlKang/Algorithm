e795483f22a6d6d63f5e92bef21d9f21021a3b28
PearlKang <rksk1588@gmail.com>
2021-10-20 00:25:32 +0900

    2021.10.20
    4892 - BufferedWriter > StringBuilder
    is better

--- Practice/BaekJoon/4892/Main.java
+++ Practice/BaekJoon/4892/Main.java
@@ -5,28 +5,25 @@ import java.io.*;
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		
 		int cnt = 0;
-
+		
 		while(true) {
 			int n0 = Integer.parseInt(br.readLine());
-
+			
 			if(n0 == 0)
 				break;
 			
-			if(n0 > 0 && n0 < 1000000) {
-				int n1 = 3 * n0;
-				
-				if(n1 % 2 == 0)
-					bw.write(String.valueOf(++cnt) + ". even " + String.valueOf(n0 / 2) + "\n");
-				else
-					bw.write(String.valueOf(++cnt) + ". odd " + String.valueOf((n0 - 1) / 2) + "\n");
-			}
+			int n1 = 3 * n0;
+			
+			if(n1 % 2 == 0)
+				sb.append(++cnt).append(". even ").append(n0 / 2).append("\n");
+			else
+				sb.append(++cnt).append(". odd ").append((n0 - 1) / 2).append("\n");
 		}
 		
 		br.close();
-		bw.flush();
-		bw.close();
+		System.out.println(sb);
 	}
 }
