606b98f57fc86bd534c058d1106dfda39af040aa
PearlKang <rksk1588@gmail.com>
2021-10-21 00:39:01 +0900

    2021.10.21
    5217 - BufferedWriter > StringBuilder
    is better

--- Practice/BaekJoon/5217/Main.java
+++ Practice/BaekJoon/5217/Main.java
@@ -6,33 +6,37 @@ import java.util.*;
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		
-		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
+		st = new StringTokenizer(br.readLine());
+
+		int T = Integer.parseInt(st.nextToken());
 		
 		if(T < 100) {
 			for(int t = 0; t < T; t++) {
-				int n = Integer.parseInt(br.readLine());
+				st = new StringTokenizer(br.readLine());
+				
+				int n = Integer.parseInt(st.nextToken());
 				
 				if(n >= 1 && n <= 12) {
-					bw.write("Pairs for " + String.valueOf(n) + ": ");
+					sb.append("Pairs for ").append(String.valueOf(n)).append(": ");
 					
 					for(int m = 1; m <= n/2; m++) {
 						if(m != n-m) {
-							bw.write(String.valueOf(m) + " " + String.valueOf(n-m));
+							sb.append(String.valueOf(m)).append(" ").append(String.valueOf(n-m));
 
 							if((m != n-m-1) && (m != n-m-2))
-								bw.write(", ");
+								sb.append(", ");
 						}
 					}
 					
-					bw.write(System.lineSeparator());
+					sb.append(System.lineSeparator());
 				}
 			}
 		}
 		
 		br.close();
-		bw.flush();
-		bw.close();
+		System.out.println(sb);
 	}
 }
