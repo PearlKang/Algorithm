881aba3fc41e8819ef8e517163df76db86b1c349
PearlKang <rksk1588@gmail.com>
2021-10-21 00:41:34 +0900

    2021.10.21
    5217 - remove StringTokenizer
    is better?

--- Practice/BaekJoon/5217/Main.java
+++ Practice/BaekJoon/5217/Main.java
@@ -1,23 +1,17 @@
 package N5217;
 
 import java.io.*;
-import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		StringTokenizer st = null;
-		st = new StringTokenizer(br.readLine());
-
-		int T = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(br.readLine());
 		
 		if(T < 100) {
 			for(int t = 0; t < T; t++) {
-				st = new StringTokenizer(br.readLine());
-				
-				int n = Integer.parseInt(st.nextToken());
+				int n = Integer.parseInt(br.readLine());
 				
 				if(n >= 1 && n <= 12) {
 					sb.append("Pairs for ").append(String.valueOf(n)).append(": ");
