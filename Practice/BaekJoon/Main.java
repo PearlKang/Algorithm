9d57c4431155423d15d996e677e808f3b285f314
PearlKang <rksk1588@gmail.com>
2021-10-22 07:12:49 +0900

    2021.10.22
    temp

---
+++ Practice/BaekJoon/Main.java
@@ -0,0 +1,20 @@
+package N;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = null;
+		st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
