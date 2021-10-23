9c972c9d42b60c7d63286d0276f457f2799e50cd
PearlKang <rksk1588@gmail.com>
2021-10-23 15:31:31 +0900

    2021.10.23
    5337

---
+++ Practice/BaekJoon/5337/Main.java
@@ -0,0 +1,9 @@
+package N5337;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
