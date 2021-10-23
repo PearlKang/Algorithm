70c4626b0c259cf923dda5302a0649c5a6fe84eb
PearlKang <rksk1588@gmail.com>
2021-10-23 15:35:19 +0900

    2021.10.23
    5338

---
+++ Practice/BaekJoon/5338/Main.java
@@ -0,0 +1,11 @@
+package N5338;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+}
