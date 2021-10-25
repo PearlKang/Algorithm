d57e0bee3929ac1113430ef8385332a797de4383
PearlKang <rksk1588@gmail.com>
2021-10-26 01:06:13 +0900

    2021.10.26
    2562 - it my best

--- Practice/BaekJoon/2562/Main.java
+++ Practice/BaekJoon/2562/Main.java
@@ -5,24 +5,20 @@ import java.io.*;
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		
-		int[] input = new int[9];
-		
-		for(int i = 0; i < 9; i++)
-			input[i] = Integer.parseInt(br.readLine());
-
 		int n = 0, cnt = 0, max = 0;
 		
-		for(int i = 0; i < 9; i++) {
-			if(input[i] > max) {
-				max = input[i];
-				cnt = i+1;
+		while(++n < 10) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if(num > max) {
+				max = num;
+				cnt = n;
 			}
 		}
 		
-		sb.append(max).append(System.lineSeparator()).append(cnt);
-		System.out.println(sb);
+		System.out.println(max);
+		System.out.println(cnt);
 		
 		br.close();
 	}
