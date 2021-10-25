2e25b6a79baa1c8d136b6a65dbc8098278fbf018
PearlKang <rksk1588@gmail.com>
2021-10-26 00:44:18 +0900

    2021.10.26
    2562 - BufferedWriter > StringBuilder
    is better

--- Practice/BaekJoon/2562/Main.java
+++ Practice/BaekJoon/2562/Main.java
@@ -1,35 +1,26 @@
 package N2562;
 
 import java.io.*;
-import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		
 		int n = 0, cnt = 0, max = 0;
 		
 		while(++n < 10) {
-			StringTokenizer st = null;
-			st = new StringTokenizer(br.readLine());
+			int num = Integer.parseInt(br.readLine());
 			
-			int num = Integer.parseInt(st.nextToken());
-			
-			if(num > 0 && num < 100) { 
-				if(num > max) {
-					max = num;
-					cnt = n;
-				}
+			if(num > max) {
+				max = num;
+				cnt = n;
 			}
 		}
 		
-		bw.write(String.valueOf(max));
-		bw.write(System.lineSeparator());
-		bw.write(String.valueOf(cnt));
+		sb.append(max).append(System.lineSeparator()).append(cnt);
+		System.out.println(sb);
 		
 		br.close();
-		bw.flush();
-		bw.close();
 	}
 }
