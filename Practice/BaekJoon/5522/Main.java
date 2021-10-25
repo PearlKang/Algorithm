e0e7f2932635d605cde46755a51acbdb5fd5781c
PearlKang <rksk1588@gmail.com>
2021-10-26 00:05:09 +0900

    2021.10.26
    5522 - is better

--- Practice/BaekJoon/5522/Main.java
+++ Practice/BaekJoon/5522/Main.java
@@ -1,25 +1,18 @@
 package N5522;
 
 import java.io.*;
-import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		StringTokenizer st = null;
+				
 		int output = 0;
 		
-		for(int i = 0; i < 5; i++) {
-			st = new StringTokenizer(br.readLine());
-			output += Integer.parseInt(st.nextToken());
-		}
+		for(int i = 0; i < 5; i++)
+			output += Integer.parseInt(br.readLine());
 		
-		bw.write(String.valueOf(output));
-		
+		System.out.println(output);
+
 		br.close();
-		bw.flush();
-		bw.close();
 	}
 }
