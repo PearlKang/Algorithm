a22342be7c76f0797ac47dbddfeac90c787437cc
PearlKang <rksk1588@gmail.com>
2021-10-28 01:43:28 +0900

    2021.10.28
    2577 - logic change

--- Practice/BaekJoon/2577/Main.java
+++ Practice/BaekJoon/2577/Main.java
@@ -7,34 +7,20 @@ class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		int A = Integer.parseInt(br.readLine());
-		int B = Integer.parseInt(br.readLine());
-		int C = Integer.parseInt(br.readLine());
-		int ABC = A*B*C;
+		int input = Integer.parseInt(br.readLine());
 		
-		int[] num = new int[10];
+		for(int i = 0; i < 2; i++)
+			input *= Integer.parseInt(br.readLine());
 		
-		for(int i = 0; i < 10; i++)
-			num[i] = 0;
+		int[] array = new int[10];
 		
-		while(ABC != 0) {
-			switch(ABC % 10) {
-			case 0: num[0]++; break;
-			case 1: num[1]++; break;
-			case 2: num[2]++; break;
-			case 3: num[3]++; break;
-			case 4: num[4]++; break;
-			case 5: num[5]++; break;
-			case 6: num[6]++; break;
-			case 7: num[7]++; break;
-			case 8: num[8]++; break;
-			case 9: num[9]++; break;
-			}
-			ABC = ABC / 10;
+		while(input != 0) {
+			array[input % 10]++;
+			input /= 10;
 		}
 		
 		for(int i = 0; i < 10; i++)
-			sb.append(num[i]).append(System.lineSeparator());
+			sb.append(array[i]).append(System.lineSeparator());
 			
 		System.out.println(sb);
 		br.close();
