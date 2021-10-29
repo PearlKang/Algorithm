6166c3e2d212159780e9d9b6775460390c31f854
PearlKang <rksk1588@gmail.com>
2021-10-29 17:41:14 +0900

    2021.10.29
    6749 - is better

--- Practice/BaekJoon/6749/Main.java
+++ Practice/BaekJoon/6749/Main.java
@@ -5,13 +5,11 @@ import java.io.*;
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		
 		int Y = Integer.parseInt(br.readLine());
 		int W = Integer.parseInt(br.readLine());
 		
-		sb.append(Y + (W - Y) * 2);
-		System.out.println(sb);
+		System.out.println(Y + (W - Y) * 2);
 		br.close();
 	}
 }
