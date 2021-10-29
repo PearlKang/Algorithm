e07baa4746bc6842baadf8db5d973e4d4027251f
PearlKang <rksk1588@gmail.com>
2021-10-29 18:18:24 +0900

    2021.10.29
    3052 - BufferedWriter > StringBuilder

--- Practice/BaekJoon/3052/Main.java
+++ Practice/BaekJoon/3052/Main.java
@@ -1,23 +1,18 @@
 package N3052;
 
 import java.io.*;
-import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 
-		StringTokenizer st = null;
-		
 		int[] array = new int[10];
 		int[] chk = new int[42];
 		int cnt = 0;
 		
-		for(int i = 0; i < 10; i++) {
-			st = new StringTokenizer(br.readLine());
-			array[i] = Integer.parseInt(st.nextToken()) % 42;
-		}
+		for(int i = 0; i < 10; i++)
+			array[i] = Integer.parseInt(br.readLine()) % 42;
 		
 		for(int i = 0; i < 10; i++)
 			chk[array[i]] = 1;
@@ -26,10 +21,8 @@ class Main {
 			if(chk[i] == 1)
 				cnt++;
 		
-		bw.write(String.valueOf(cnt));
-		
+		sb.append(cnt);
+		System.out.println(sb);
 		br.close();
-		bw.flush();
-		bw.close();
 	}
 }
