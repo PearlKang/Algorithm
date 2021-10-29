bbe57c4c0f022d418f9449b4d84ff79c7bd6c04c
PearlKang <rksk1588@gmail.com>
2021-10-30 02:55:35 +0900

    2021.10.30
    6778 -
    simple version
    its time best

--- Practice/BaekJoon/6778/Main.java
+++ Practice/BaekJoon/6778/Main.java
@@ -5,25 +5,19 @@ import java.io.*;
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		
 		int antenna = Integer.parseInt(br.readLine());
 		int eyes = Integer.parseInt(br.readLine());
 		
-		if(antenna >= 3 && eyes <= 4) {
-			sb.append("TroyMartian");
-			sb.append(System.lineSeparator());
-		}
+		if(antenna >= 3 && eyes <= 4)
+			System.out.println("TroyMartian");
 		
-		if(antenna <= 6 && eyes >= 2) {
-			sb.append("VladSaturnian");
-			sb.append(System.lineSeparator());
-		}
+		if(antenna <= 6 && eyes >= 2)
+			System.out.println("VladSaturnian");
 		
 		if(antenna <= 2 && eyes <= 3)
-			sb.append("GraemeMercurian");
+			System.out.println("GraemeMercurian");
 		
-		System.out.print(sb);
 		br.close();
 	}
 }
