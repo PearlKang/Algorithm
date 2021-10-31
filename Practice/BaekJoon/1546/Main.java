6b07936408a0bf17b415d3d4ac700ae124f4289b
PearlKang <rksk1588@gmail.com>
2021-11-01 00:38:52 +0900

    2021.11.01
    1546 - my default style
    BufferedReader
    BufferedWriter
    StringTokenizer

---
+++ Practice/BaekJoon/1546/Main.java
@@ -0,0 +1,42 @@
+package N1546;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		
+		if(N <= 1000) {
+			double[] score = new double[N];
+			double M = 0, sum = 0;
+			
+			st = new StringTokenizer(br.readLine());
+			
+			for(int n = 0; n < N; n++) {
+				score[n] = Integer.parseInt(st.nextToken());
+				
+				if(M < score[n])
+					M = score[n];
+			}
+
+			for(int n = 0; n < N; n++)
+				score[n] = (score[n] / M) * 100;
+			
+			for(int n = 0; n < N; n++)
+				sum += score[n];
+			
+			bw.write(String.valueOf(sum/N));
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
