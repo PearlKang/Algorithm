9f18228e9fec59ff9c5d253e9addcc377057ec66
PearlKang <rksk1588@gmail.com>
2021-11-01 00:57:15 +0900

    2021.11.01
    1546 -
    use String.split
    is better...???????

--- Practice/BaekJoon/1546/Main.java
+++ Practice/BaekJoon/1546/Main.java
@@ -1,41 +1,25 @@
 package N1546;
 
 import java.io.*;
-import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		
-		StringTokenizer st = null;
+		int N = Integer.parseInt(br.readLine());
+		String[] score = br.readLine().split(" ");
 		
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
+		double M = 0, sum = 0;
 		
-		if(N <= 1000) {
-			double[] score = new double[N];
-			double M = 0, sum = 0;
-			
-			st = new StringTokenizer(br.readLine());
-			
-			for(int n = 0; n < N; n++) {
-				score[n] = Integer.parseInt(st.nextToken());
-				
-				if(M < score[n])
-					M = score[n];
-			}
+		for(int n = 0; n < N; n++)
+			if(M < Integer.parseInt(score[n]))
+				M = Integer.parseInt(score[n]);
 
-			for(int n = 0; n < N; n++)
-				score[n] = (score[n] / M) * 100;
-			
-			for(int n = 0; n < N; n++)
-				sum += score[n];
-			
-			sb.append(sum/N);
-		}
+		for(int n = 0; n < N; n++)
+			sum += Integer.parseInt(score[n]) * 100 / M;
+
+		System.out.println(sum/N);
 		
-		System.out.println(sb);
 		br.close();
 	}
 }
