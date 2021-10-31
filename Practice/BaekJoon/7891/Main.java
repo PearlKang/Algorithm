f76b596cba0bff00755df6941c29439163462419
PearlKang <rksk1588@gmail.com>
2021-10-31 13:58:51 +0900

    2021.10.31
    7891 -
    remove StringBuilder

--- Practice/BaekJoon/7891/Main.java
+++ Practice/BaekJoon/7891/Main.java
@@ -5,16 +5,13 @@ import java.io.*;
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		
 		int t = Integer.parseInt(br.readLine());
 		
 		for(int i = 0; i < t; i++) {
 			String[] input = br.readLine().split(" ");
-			sb.append(Integer.parseInt(input[0]) + Integer.parseInt(input[1])).append(System.lineSeparator());
+			System.out.println(Integer.parseInt(input[0]) + Integer.parseInt(input[1]));
 		}
-		
-		System.out.println(sb);
 		br.close();
 	}
 }
