a5dafb84a3e21ff7c7c2df1b67c49b98f3f64509
PearlKang <rksk1588@gmail.com>
2021-11-03 21:04:25 +0900

    2021.11.03
    8958 - is my best

--- Practice/BaekJoon/8958/Main.java
+++ Practice/BaekJoon/8958/Main.java
@@ -5,24 +5,23 @@ import java.io.*;
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(br.readLine());
 		
 		for(int n = 0; n < N; n++) {
 			String input = br.readLine();
-			int length = input.length();
 			int continuous = 0, score = 0;
 			
-			if(length > 0 && length < 80) {
-				for(int i = 0; i < length; i++) {
-					if(input.charAt(i) == 'O')
-						score += ++continuous;
-					else
-						continuous = 0;
-				}
+			for(int i = 0; i < input.length(); i++) {
+				if(input.charAt(i) == 'O')
+					score += ++continuous;
+				else
+					continuous = 0;
 			}
-			System.out.println(score);
+			sb.append(score).append(System.lineSeparator());
 		}
+		System.out.println(sb);
 		br.close();
 	}
 }
