ed20c5af1c3f9011bc04dd4ce4d82acae31835d2
PearlKang <rksk1588@gmail.com>
2021-11-04 21:39:46 +0900

    2021.11.04
    4344 -
    remove StringTokenizer
    use split

--- Practice/BaekJoon/4344/Main.java
+++ Practice/BaekJoon/4344/Main.java
@@ -1,37 +1,30 @@
 package N4344;
 
 import java.io.*;
-import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		StringTokenizer st = null;
-		
-		st = new StringTokenizer(br.readLine());
-		int C = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(br.readLine());
 		
 		for(int c = 0; c < C; c++) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int[] score = new int[N];
+			String[] input = br.readLine().split(" ");
+			int N = Integer.parseInt(input[0]);
 			int sum = 0;
 			double cnt = 0;
 			
 			if(N < 1 || N > 1000)
 				continue;
 			
-			for(int n = 0; n < N; n++) {
-				score[n] = Integer.parseInt(st.nextToken());
-				sum += score[n];
-			}
+			for(int n = 1; n <= N; n++)
+				sum += Integer.parseInt(input[n]);
 			
 			double avg = sum / N;
 			
-			for(int n = 0; n < N; n++)
-				if(score[n] > avg)
+			for(int n = 1; n <= N; n++)
+				if(Integer.parseInt(input[n]) > avg)
 					cnt++;
 			
 			bw.write(String.format("%.3f", cnt*100/N) + "%");
