af3ff822379437e3222409e9b7c2639183449dce
PearlKang <rksk1588@gmail.com>
2021-11-06 13:51:05 +0900

    2021.11.06
    4673

--- Practice/BaekJoon/4673/Main.java
+++ Practice/BaekJoon/4673/Main.java
@@ -6,9 +6,12 @@ class Main {
 		
 		boolean[] chk = new boolean[10001];
 		
-		for(int i = 1; i <= 10000; i++)
-			if(dn(i) <= 10000)
-				chk[dn(i)] = true;
+		for(int i = 1; i <= 10000; i++) {
+			int dn = dn(i);
+			
+			if(dn <= 10000)
+				chk[dn] = true;
+		}
 		
 		for(int i = 1; i <= 10000; i++)
 			if(!chk[i])
