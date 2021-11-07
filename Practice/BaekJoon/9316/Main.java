e08133c2ca0921041a037aba6c281acd7d036217
PearlKang <rksk1588@gmail.com>
2021-11-07 21:33:32 +0900

    2021.11.07
    9316 -
    remove StringTokenizer

--- Practice/BaekJoon/9316/Main.java
+++ Practice/BaekJoon/9316/Main.java
@@ -1,17 +1,13 @@
 package N9316;
 
 import java.io.*;
-import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		StringTokenizer st = null;
-		
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
 		
 		for(int n = 0; n < N; n++)
 			sb.append("Hello World, Judge ").append(n+1).append("!").append(System.lineSeparator());
