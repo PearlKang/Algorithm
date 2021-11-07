bdabafbc4c76f82d1251d0a60d231be649edcb4f
PearlKang <rksk1588@gmail.com>
2021-11-07 21:32:25 +0900

    2021.11.07
    9316 -
    remove BufferedWriter
    use StringBuilder
    use StringTokenizer

--- Practice/BaekJoon/9316/Main.java
+++ Practice/BaekJoon/9316/Main.java
@@ -1,23 +1,22 @@
 package N9316;
 
 import java.io.*;
+import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		
-		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
 		
-		for(int n = 0; n < N; n++) {
-			bw.write("Hello World, Judge ");
-			bw.write(String.valueOf(n+1));
-			bw.write("!");
-			bw.write(System.lineSeparator());
-		}
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
 		
-		br.close();
-		bw.flush();
-		bw.close();
+		for(int n = 0; n < N; n++)
+			sb.append("Hello World, Judge ").append(n+1).append("!").append(System.lineSeparator());
+
+		System.out.print(sb);
+		br.close();		
 	}
 }
