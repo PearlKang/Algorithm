fe1e3265cec6a4f49b5f52028e962befb84c2e86
PearlKang <rksk1588@gmail.com>
2021-11-08 20:57:57 +0900

    2021.11.08
    10170 -
    use StringBuilder

--- Practice/BaekJoon/10170/Main.java
+++ Practice/BaekJoon/10170/Main.java
@@ -2,18 +2,22 @@ package N10170;
 
 class Main {
 	public static void main(String[] args) {
-		System.out.println("NFC West       W   L  T");
-		System.out.println("-----------------------");
-		System.out.println("Seattle        13  3  0");
-		System.out.println("San Francisco  12  4  0");
-		System.out.println("Arizona        10  6  0");
-		System.out.println("St. Louis      7   9  0");
-		System.out.println("");
-		System.out.println("NFC North      W   L  T");
-		System.out.println("-----------------------");
-		System.out.println("Green Bay      8   7  1");
-		System.out.println("Chicago        8   8  0");
-		System.out.println("Detroit        7   9  0");
-		System.out.println("Minnesota      5  10  1");
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("NFC West       W   L  T").append(System.lineSeparator());
+		sb.append("-----------------------").append(System.lineSeparator());
+		sb.append("Seattle        13  3  0").append(System.lineSeparator());
+		sb.append("San Francisco  12  4  0").append(System.lineSeparator());
+		sb.append("Arizona        10  6  0").append(System.lineSeparator());
+		sb.append("St. Louis      7   9  0").append(System.lineSeparator());
+		sb.append(System.lineSeparator());
+		sb.append("NFC North      W   L  T").append(System.lineSeparator());
+		sb.append("-----------------------").append(System.lineSeparator());
+		sb.append("Green Bay      8   7  1").append(System.lineSeparator());
+		sb.append("Chicago        8   8  0").append(System.lineSeparator());
+		sb.append("Detroit        7   9  0").append(System.lineSeparator());
+		sb.append("Minnesota      5  10  1").append(System.lineSeparator());
+		
+		System.out.print(sb);
 	}
 }
