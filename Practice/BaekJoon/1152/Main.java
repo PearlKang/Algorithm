8be47fecf5277813dbd16266c94c1983d59ed6e3
PearlKang <rksk1588@gmail.com>
2021-11-12 21:38:53 +0900

    2021.11.12
    1152
    remove StringBuilder

--- Practice/BaekJoon/1152/Main.java
+++ Practice/BaekJoon/1152/Main.java
@@ -6,14 +6,11 @@ import java.util.*;
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
 		StringTokenizer st = null;
 		
 		st = new StringTokenizer(br.readLine());
-		sb.append(st.countTokens());
+		System.out.print(st.countTokens());
 		
-		System.out.print(sb);
 		br.close();
 	}
 }
