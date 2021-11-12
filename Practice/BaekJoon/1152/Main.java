ab3b83a49a6e62fd4f0e476fb67505601c167334
PearlKang <rksk1588@gmail.com>
2021-11-12 21:37:42 +0900

    2021.11.12
    1152
    remove BufferedWriter
    use StringBuilder

--- Practice/BaekJoon/1152/Main.java
+++ Practice/BaekJoon/1152/Main.java
@@ -6,15 +6,14 @@ import java.util.*;
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		
 		StringTokenizer st = null;
 		
 		st = new StringTokenizer(br.readLine());
-		bw.write(String.valueOf(st.countTokens()));
+		sb.append(st.countTokens());
 		
-		bw.flush();
-		bw.close();
+		System.out.print(sb);
 		br.close();
 	}
 }
