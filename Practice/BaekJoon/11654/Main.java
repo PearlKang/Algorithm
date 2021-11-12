1dc6fc2ff111b6a19c8f5046366c604036fb1e64
PearlKang <rksk1588@gmail.com>
2021-11-12 21:28:02 +0900

    2021.11.12
    11654

--- Practice/BaekJoon/11654/Main.java
+++ Practice/BaekJoon/11654/Main.java
@@ -1,19 +1,7 @@
 package N11654;
 
-import java.io.*;
-import java.util.*;
-
 class Main {
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = null;
-		
-		st = new StringTokenizer(br.readLine());
-		int input = st.nextToken().charAt(0);
-		
-		System.out.println(input);
-		
-		br.close();
+		System.out.print(System.in.read());
 	}
 }
