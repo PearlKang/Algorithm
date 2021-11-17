da41c77a994d76be745ffa599b614eae9bf9f7a2
PearlKang <rksk1588@gmail.com>
2021-11-17 18:08:19 +0900

    2021.11.17
    10817

--- Practice/BaekJoon/10817/Main.java
+++ Practice/BaekJoon/10817/Main.java
@@ -8,11 +8,17 @@ class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String[] input = br.readLine().split(" ");
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(br.readLine());
+		int[] input = new int[3];
+		
+		for(int i = 0; i < 3; i++)
+			input[i] = Integer.parseInt(st.nextToken());
 		
 		Arrays.sort(input);
 		
-		bw.write(input[1]);
+		bw.write(String.valueOf(input[1]));
 		
 		bw.flush();
 		bw.close();
