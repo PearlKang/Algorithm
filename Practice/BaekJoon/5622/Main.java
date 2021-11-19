c622178b16cd94f1955204082c22bd37eefd174a
PearlKang <rksk1588@gmail.com>
2021-11-19 21:10:21 +0900

    2021.11.19
    5622 - S...

--- Practice/BaekJoon/5622/Main.java
+++ Practice/BaekJoon/5622/Main.java
@@ -1,28 +1,26 @@
 package N5622;
 
 import java.io.*;
-import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		StringTokenizer st = null;
+		String input = br.readLine();
 		
-		st = new StringTokenizer(br.readLine());
-		String input = st.nextToken();
-		
+		int length = input.length();
 		int cnt = 0;
-
-		for(int i  = 0; i < input.length(); i++) {
-			char tmp = input.charAt(i);
-			cnt += (tmp - 'A') / 3 + 3;
-			if(tmp == 'Z')
-				cnt--;
+		
+		if(length >= 2 && length <= 15) {
+			for(int i  = 0; i < length; i++) {
+				char tmp = input.charAt(i);
+				cnt += (tmp - 'A') / 3 + 3;
+				if(tmp == 'Z')
+					cnt--;
+			}
+			bw.write(String.valueOf(cnt));
 		}
-		bw.write(String.valueOf(cnt));
-
 		bw.flush();
 		bw.close();
 		br.close();
