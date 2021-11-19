5fe57c69e916b0370f1cd972838681fb60d88da3
PearlKang <rksk1588@gmail.com>
2021-11-19 21:20:31 +0900

    2021.11.19
    5622 - my default style

--- Practice/BaekJoon/5622/Main.java
+++ Practice/BaekJoon/5622/Main.java
@@ -1,26 +1,69 @@
 package N5622;
 
 import java.io.*;
+import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String input = br.readLine();
+		StringTokenizer st = null;
 		
-		int length = input.length();
+		st = new StringTokenizer(br.readLine());
+		String input = st.nextToken();
+        
 		int cnt = 0;
-		
-		if(length >= 2 && length <= 15) {
-			for(int i  = 0; i < length; i++) {
-				char tmp = input.charAt(i);
-				cnt += (tmp - 'A') / 3 + 3;
-				if(tmp == 'Z')
-					cnt--;
+		int length = input.length();
+        
+		for(int i = 0; i < length; i++) {
+			switch(input.charAt(i)) {
+			case 'A':
+			case 'B':
+			case 'C': 
+				cnt += 3; 
+				break;
+			case 'D':
+			case 'E':
+			case 'F': 
+				cnt += 4; 
+				break;
+			case 'G':
+			case 'H':
+			case 'I': 
+				cnt += 5; 
+				break;
+			case 'J':
+			case 'K':
+			case 'L': 
+				cnt += 6; 
+				break;
+			case 'M':
+			case 'N':
+			case 'O': 
+				cnt += 7; 
+				break;
+			case 'P':
+			case 'Q':
+			case 'R':
+			case 'S':
+				cnt += 8; 
+				break;
+			case 'T':
+			case 'U':
+			case 'V': 
+				cnt += 9; 
+				break;
+			case 'W':
+			case 'X':
+			case 'Y':
+			case 'Z': 
+				cnt += 10; 
+				break;
 			}
-			bw.write(String.valueOf(cnt));
 		}
+		bw.write(String.valueOf(cnt));
+		
 		bw.flush();
 		bw.close();
 		br.close();
