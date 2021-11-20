737e5edee11bb8751527c6261c424bf2a362cc7f
PearlKang <rksk1588@gmail.com>
2021-11-20 17:24:13 +0900

    2021.11.20
    2941

--- Practice/BaekJoon/2941/Main.java
+++ Practice/BaekJoon/2941/Main.java
@@ -1,26 +1,17 @@
 package N2941;
 
 import java.io.*;
-import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		StringTokenizer st = null;
-		
-		st = new StringTokenizer(br.readLine());
-		String input = st.nextToken();
+		String input = br.readLine();
 		String[] array = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 		
-		for(int i = 0; i < 8; i++)
-			input = input.replace(array[i], " ");
+		for(String s:array)
+			input = input.replace(s, " ");
 
-		bw.write(String.valueOf(input.length()));
-		
-		bw.flush();
-		bw.close();
+		System.out.print(input.length());
 		br.close();
 	}
 }
