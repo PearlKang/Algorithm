e59529d8819b5a4e74c9716014f0e93842f2148e
PearlKang <rksk1588@gmail.com>
2021-11-22 20:13:07 +0900

    2021.11.22
    10926

---
+++ Practice/BaekJoon/10926/Main.java
@@ -0,0 +1,26 @@
+package N10926;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		String input = st.nextToken();
+		int length = input.length();
+		
+		if(length <= 50) {
+			bw.write(input + "??!");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
