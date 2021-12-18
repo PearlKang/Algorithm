4796ddfcb46f0ebf016257a112ba97ba1c4ee367
PearlKang <rksk1588@gmail.com>
2021-12-19 02:23:01 +0900

    2021.12.19
    9020

--- Practice/BaekJoon/9020/Main.java
+++ Practice/BaekJoon/9020/Main.java
@@ -3,7 +3,9 @@ package N9020;
 import java.io.*;
 import java.util.*;
 
-class Main {
+public class Main {
+	public static boolean[] prime = new boolean[10001];
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -13,14 +15,24 @@ class Main {
 		st = new StringTokenizer(br.readLine());
 		int T = Integer.parseInt(st.nextToken());
 		
+		isPrime();
+		
 		for(int t = 0; t < T; t++) {
 			st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			
 			if(n >= 4 && n <= 10000) {
-				int goldbachP = goldbachPartition(n);
-				bw.write(String.valueOf(goldbachP) + " " + String.valueOf(n-goldbachP));
-				bw.write(System.lineSeparator());
+				int front = n/2;
+				int back = n/2;
+				while(true) {
+					if(!prime[front] && !prime[back]) {
+						bw.write(String.valueOf(front) + " " + String.valueOf(back));
+						bw.write(System.lineSeparator());
+						break;
+					}
+					front--;
+					back++;
+				}
 			}
 		}
 		
@@ -28,31 +40,15 @@ class Main {
 		bw.close();
 		br.close();
 	}
-	
-	public static boolean isPrime(int input) {
-		if(input == 1)
-			return false;
-				
-		for(int i = 2; i <= Math.sqrt(input); i++)
-			if(input % i == 0)
-				return false;
+	//Sieve Of Eratosthenes
+	public static void isPrime() {
+		prime[0] = prime[1] = true;
 		
-		return true;
-	}
-	
-	public static int goldbachPartition(int input) {
-		int min = 10000;
-		int output = 0;
-		
-		for(int i = 0; i <= input/2; i++) {
-			if(isPrime(i) && isPrime(input-i)) {
-				if(min > Math.abs(input-2*i)) {
-					min = Math.abs(input-2*i);
-					output = i;
-				}
-			}
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if(prime[i])
+				continue;
+			for(int j = i*i; j < prime.length; j+=i)
+				prime[j] = true;
 		}
-		
-		return output;
 	}
 }
