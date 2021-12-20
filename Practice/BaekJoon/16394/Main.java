7c1656acdf930741adf2e8358d66a9b32668bf28
PearlKang <rksk1588@gmail.com>
2021-12-21 00:56:53 +0900

    2021.12.21
    16394

---
+++ Practice/BaekJoon/16394/Main.java
@@ -0,0 +1,12 @@
+package N16394;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		System.out.println(input - 1946);
+		sc.close();
+	}
+}
