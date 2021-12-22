6198bc5e20d4e0b19787fd40874630bc8fb8a3bd
PearlKang <rksk1588@gmail.com>
2021-12-23 00:28:15 +0900

    2021.12.23
    1085

--- Practice/BaekJoon/1085/Main.java
+++ Practice/BaekJoon/1085/Main.java
@@ -16,16 +16,8 @@ class Main {
 		int w = Integer.parseInt(st.nextToken());
 		int h = Integer.parseInt(st.nextToken());
 		
-		if((w >= 1 && w <= 1000) && (h >= 1 && h <= 1000) && (x >= 1 && x <= w-1) && (y >= 1 && y <= h-1)) {
-			if(x <= w-x && x <= y && x <= h-y)
-				bw.write(String.valueOf(x));
-			else if(w-x <= x && w-x <= y && w-x <= h-y)
-				bw.write(String.valueOf(w-x));
-			else if(y <= x && y <= w-x && y <= h-y)
-				bw.write(String.valueOf(y));
-			else if(h-y <= x && h-y <= w-x && h-y <= y)
-				bw.write(String.valueOf(h-y));
-		}
+		if((w >= 1 && w <= 1000) && (h >= 1 && h <= 1000) && (x >= 1 && x <= w-1) && (y >= 1 && y <= h-1))
+			bw.write(String.valueOf(Math.min(Math.min(x, y), Math.min(w-x, h-y))));
 		
 		bw.flush();
 		bw.close();
