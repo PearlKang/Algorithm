a9f491a34ada22085a8e15d6716ae43a9d1d6ddb
PearlKang <rksk1588@gmail.com>
2022-01-26 00:35:26 +0900

    2022.01.26

--- Practice/BaekJoon/10814/Main.java
+++ Practice/BaekJoon/10814/Main.java
@@ -11,23 +11,22 @@ public class Main {
 		
 		st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
-		String[][] array = new String[N][2];
+		Person[] person = new Person[N];
 		
 		if(N >= 1 && N <= 100000) {
 			for(int n = 0; n < N; n++) {
 				st = new StringTokenizer(br.readLine());
-				array[n][0] = st.nextToken();
-				array[n][1] = st.nextToken();
+				person[n] = new Person(Integer.parseInt(st.nextToken()), st.nextToken());
 			}
 			
-			Arrays.sort(array, new Comparator<String[]>() {
-				public int compare(String[] s1, String[] s2) {
-					return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+			Arrays.sort(person, new Comparator<Person>() {
+				public int compare(Person p1, Person p2) {
+					return p1.age - p2.age;
 				}
 			});
 			
 			for(int n = 0; n < N; n++) {
-				bw.write(array[n][0] + " " + array[n][1]);
+				bw.write(person[n].age + " " + person[n].name);
 				bw.write(System.lineSeparator());
 			}
 		}
@@ -35,5 +34,15 @@ public class Main {
 		bw.flush();
 		bw.close();
 		br.close();
+	}
+	
+	public static class Person {
+		int age;
+		String name;
+		
+		public Person(int age, String name) {
+			this.age = age;
+			this.name = name;
+		}
 	}
 }
