53ed5e668f84a06b8719a477cef0e70598d7ccdd
PearlKang <rksk1588@gmail.com>
2022-01-29 02:51:06 +0900

    2022.01.29

--- Practice/BaekJoon/2748/Main.java
+++ Practice/BaekJoon/2748/Main.java
@@ -4,8 +4,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static long[] array;
-	
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -14,29 +12,23 @@ public class Main {
 		
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-		array = new long[n+1];
-		
-		for(int i = 0; i < array.length; i++) {
-			array[i] = -1;
-		}
-		
-		array[0] = 0;
-		array[1] = 1;
 		
 		if(n > 0 && n <= 90) {
-			bw.write(String.valueOf(fibonacci(n)));
+			long f1 = 0;
+			long f2 = 1;
+			long sum = f1 + f2;
+			
+			for(int i = 1; i < n; i++) {
+				sum = f1 + f2;
+				f1 = f2;
+				f2 = sum;
+			}
+			
+			bw.write(String.valueOf(sum));
 		}
 		
 		bw.flush();
 		bw.close();
 		br.close();
-	}
-	
-	public static long fibonacci(int input) {
-		if(array[input] == -1) {
-			array[input] = fibonacci(input-1) + fibonacci(input-2);
-		}
-		
-		return array[input];
 	}
 }
