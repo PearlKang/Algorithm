a5c81b59471dacf9dd6f20ca74f3dca7298e40b4
PearlKang <rksk1588@gmail.com>
2022-01-30 02:22:44 +0900

    2022.01.30

--- Practice/BaekJoon/1003/Main.java
+++ Practice/BaekJoon/1003/Main.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	public static Integer[][] dp = new Integer[41][2];
+	
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -12,30 +14,34 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		int T = Integer.parseInt(st.nextToken());
 		
-		int[][] fibonacci = new int[41][2];
-		
-		fibonacci[0][0] = 1;
-		fibonacci[0][1] = 0;
-		fibonacci[1][0] = 0;
-		fibonacci[1][1] = 1;
+		dp[0][0] = 1;
+		dp[0][1] = 0;
+		dp[1][0] = 0;
+		dp[1][1] = 1;
 		
 		for(int t = 0; t < T; t++) {
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			
-			for(int i = 2; i <= N; i++) {
-				fibonacci[i][0] = fibonacci[i-1][0] + fibonacci[i-2][0];
-				fibonacci[i][1] = fibonacci[i-1][1] + fibonacci[i-2][1];
-			}
+			fibonacci(N);
 			
-			bw.write(String.valueOf(fibonacci[N][0]));
+			bw.write(String.valueOf(dp[N][0]));
 			bw.write(" ");
-			bw.write(String.valueOf(fibonacci[N][1]));
+			bw.write(String.valueOf(dp[N][1]));
 			bw.write(System.lineSeparator());
 		}
 		
 		bw.flush();
 		bw.close();
 		br.close();
+	}
+	
+	public static Integer[] fibonacci(int input) {
+		if(dp[input][0] == null || dp[input][1] == null) {
+			dp[input][0] = fibonacci(input-1)[0] + fibonacci(input-2)[0];
+			dp[input][1] = fibonacci(input-1)[1] + fibonacci(input-2)[1];
+		}
+		
+		return dp[input];
 	}
 }
