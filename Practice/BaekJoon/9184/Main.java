b800075d36ee3c7a30a950b758128f7c1963c95d
PearlKang <rksk1588@gmail.com>
2022-02-01 00:33:02 +0900

    2022.02.01

--- Practice/BaekJoon/9184/Main.java
+++ Practice/BaekJoon/9184/Main.java
@@ -4,32 +4,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	public static StringTokenizer st = null;
-	
-	public static int a;
-	public static int b;
-	public static int c;
-	
 	public static void main(String args[]) throws Exception {
-		while(input()) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		boolean check = a == -1 && b == -1 && c == -1 ? false : true;
+		
+		while(check) {
 			bw.write(String.valueOf(w(a, b, c)));
 			bw.write(System.lineSeparator());
+			
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			check = a == -1 && b == -1 && c == -1 ? false : true;
 		}
 		
 		bw.flush();
 		bw.close();
 		br.close();
-	}
-	
-	public static boolean input() throws Exception {
-		st = new StringTokenizer(br.readLine());
-		a = Integer.parseInt(st.nextToken());
-		b = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		
-		return a == -1 && b == -1 && c == -1 ? false : true;
 	}
 	
 	public static int w(int a1, int b1, int c1) {
