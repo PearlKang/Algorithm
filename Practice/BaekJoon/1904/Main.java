9e91f32720208a2cf4d341f5ca3e812deed9fc53
PearlKang <rksk1588@gmail.com>
2022-02-02 02:25:45 +0900

    2022.02.02

--- Practice/BaekJoon/1904/Main.java
+++ Practice/BaekJoon/1904/Main.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	public static int[] dp;
+	
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -13,21 +15,25 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		
 		if(N >= 1 && N <= 1000000) {
-			int[] dp = new int[N+1];
+			dp = new int[N+1];
 			
 			dp[0] = 0;
 			dp[1] = 1;
 			dp[2] = 2;
 			
-			for(int n = 3; n <= N; n++) {
-				dp[n] = dp[n-1] + dp[n-2];
-			}
-			
-			bw.write(String.valueOf(dp[N] % 15746));
+			bw.write(String.valueOf(Tile(N)));
 		}
 		
 		bw.flush();
 		bw.close();
 		br.close();
+	}
+	
+	public static int Tile(int input) {
+		if(input != 0 && dp[input] == 0) {
+			dp[input] = (Tile(input-1) + Tile(input-2)) % 15746;
+		}
+		
+		return dp[input];
 	}
 }
