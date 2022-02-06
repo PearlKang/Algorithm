8a7a48851c4fdfa993aa2608c98d2ab062f2b337
PearlKang <rksk1588@gmail.com>
2022-02-06 17:43:09 +0900

    2022.02.06

--- Practice/BaekJoon/1149/Main.java
+++ Practice/BaekJoon/1149/Main.java
@@ -4,9 +4,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int[][] rgb;
-	public static int[][] dp;
-	
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -14,10 +11,8 @@ public class Main {
 		
 		st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
+		int[][] rgb = new int[N][3];
 		
-		rgb = new int[N][3];
-		dp = new int[N][3];
-
 		if(N >= 2 && N <= 1000) {
 			for(int n = 0; n < N; n++) {
 				st = new StringTokenizer(br.readLine());
@@ -26,31 +21,17 @@ public class Main {
 				rgb[n][2] = Integer.parseInt(st.nextToken());
 			}
 			
-			dp[0][0] = rgb[0][0];
-			dp[0][1] = rgb[0][1];
-			dp[0][2] = rgb[0][2];
+			for(int n = 1; n < N; n++) {
+				rgb[n][0] += Math.min(rgb[n-1][1], rgb[n-1][2]);
+				rgb[n][1] += Math.min(rgb[n-1][0], rgb[n-1][2]);
+				rgb[n][2] += Math.min(rgb[n-1][0], rgb[n-1][1]);
+			}
 			
-			bw.write(String.valueOf(Math.min(Color(N-1, 0), (Math.min(Color(N-1, 1), Color(N-1, 2))))));
+			bw.write(String.valueOf(Math.min(rgb[N-1][0], (Math.min(rgb[N-1][1], rgb[N-1][2])))));
 		}
 		
 		bw.flush();
 		bw.close();
 		br.close();
-	}
-	
-	public static int Color(int n, int input) {
-		if(dp[n][input] == 0) {
-			if(input == 0) {
-				dp[n][0] = Math.min(Color(n-1, 1), Color(n-1, 2)) + rgb[n][0];
-			}
-			if(input == 1) {
-				dp[n][1] = Math.min(Color(n-1, 0), Color(n-1, 2)) + rgb[n][1];
-			}
-			if(input == 2) {
-				dp[n][2] = Math.min(Color(n-1, 0), Color(n-1, 1)) + rgb[n][2];
-			}
-		}
-		
-		return dp[n][input];
 	}
 }
